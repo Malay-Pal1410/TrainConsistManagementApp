9bf97fbcee2dbfd9874c577fc0bc058baf1fe947
Malay-Pal1410 <malaypal954@gmail.com>
2026-04-06 10:00:51 +0530

    UC1:Initialize Train and Display Consist Summary.

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -19,6 +19,5 @@ public class TrainConsistManagement {
         System.out.println("Current Train Consist: " + trainConsist);
 
         System.out.println("System ready for operations...");
-        System.out.println();
     }
 }
